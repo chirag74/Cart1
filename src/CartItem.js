637724f4939e85ceac9971f07d60f26c50be08cd
chirag74 <chiragkumar31081999@gmail.com>
2021-04-24 16:04:41 +0530

    inc quantity

--- src/CartItem.js
+++ src/CartItem.js
@@ -11,6 +11,10 @@ class CartItem extends React.Component {
 
 
         }
+        
+    }
+    increaseQuantity= ()=>{
+      console.log('this',this.state);
     }
     render(){
         const {price,title,qty}=this.state;
@@ -25,7 +29,7 @@ class CartItem extends React.Component {
 <div style={{color:'#777'}}>{price}</div>
 <div style={{color:'#777'}}>{qty}</div>
 <div className="cart-item-actions">
-    <img alt="inc" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/992/992651.svg?token=exp=1619094877~hmac=cd3082df510fe9e16bbcd488548f781d"></img>
+    <img alt="inc"  className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/992/992651.svg?token=exp=1619094877~hmac=cd3082df510fe9e16bbcd488548f781d" onClick={this.increaseQuantity} ></img>
     <img alt="dec" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/1828/1828906.svg?token=exp=1619094832~hmac=bd767986253e3a249c99418336339027"></img>
     <img alt="dlt" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/3439/3439691.svg?token=exp=1619094908~hmac=0b179b38d2988d1d87c71dd69cc1f457"></img>
 </div>
